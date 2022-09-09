2b833b8c23740884f7bdf8a28afb0abb6e2611f5
Matt Porter <matthew.ellis.porter@gmail.com>
2022-09-09 13:44:50 -0500

    express set up

---
+++ server.js
@@ -0,0 +1,32 @@
+// DECLARE REQUIREMENTS
+const express = require("express"); 
+const app = express();
+require('dotenv').config();
+
+let port = process.env.port
+
+// DATABASE
+
+// MIDDLEWARE
+
+// CONTROLLERS
+
+// Index
+
+// New
+
+// Delete
+
+// Update
+
+// Create
+
+// Edit
+
+// Show
+
+// Listener
+
+app.listen(port, function(){
+    console.log(port)
+})
