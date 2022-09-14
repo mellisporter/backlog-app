efe72194f230946af4063a6764662ba7e59b218c
Matt Porter <matthew.ellis.porter@gmail.com>
2022-09-14 11:39:04 -0400

    seed data updated

--- models/gameSeed.js
+++ models/gameSeed.js
@@ -1,11 +1,36 @@
 gameSeed = [{
-    title: 'Cracking the Coding Interview',
-    console: 'Gayle Laakmann McDowell',
-    releaseDate: '2012-08-08',
+    title: 'Super Mario Bros. 3',
+    console: 'SNES',
+    releaseDate: '1988-10-23',
     coverURL: "https://upload.wikimedia.org/wikipedia/en/a/a5/Super_Mario_Bros._3_coverart.png",
-    review: "This game is great!",
+    review: "This is the best platformers I have played. A true classic that enhances the SMB formula.",
     hasBeaten: true,
 },
+{
+    title: 'Halo 2',
+    console: 'Xbox',
+    releaseDate: '2004-11-09',
+    coverURL: "https://m.media-amazon.com/images/I/51AXK29H5BL.jpg",
+    review: "An excellent campaign, but this game is at its best when you're playing with friends.",
+    hasBeaten: false,
+},
+{
+    title: 'Final Fantasy IX',
+    console: 'PSOne',
+    releaseDate: '2000-07-07',
+    coverURL: "https://upload.wikimedia.org/wikipedia/en/5/51/Ffixbox.jpg",
+    review: "Excellent combat system that improves upon the formula of FFVII. The last great PSOne RPG",
+    hasBeaten: false,
+},
+{
+    title: 'Elden Ring',
+    console: 'PS5',
+    releaseDate: '2022-02-25',
+    coverURL: "https://cdn2.spong.com/pack/e/l/eldenring455877l/_-Elden-Ring-PS4-_.jpg",
+    review: "This game ruined gaming for me. It's player exploration, choice, and combat is unmatched. No game has felt good since.",
+    hasBeaten: true,
+},
+
 ];
 
 module.exports= gameSeed;
