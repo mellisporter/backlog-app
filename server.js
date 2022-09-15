1cd0efaaaf5572ca1977e15f1389eea5ef4dbf1d
Matt Porter <matthew.ellis.porter@gmail.com>
2022-09-14 21:14:01 -0400

    fixed for heroku

--- server.js
+++ server.js
@@ -153,6 +153,6 @@ app.get('/logout', function(req, res, next) {
 // })
 // Listener
 
-app.listen(port || function(){
+app.listen(port || 3000, function(){
     console.log("Ready to game on Port " + port)
 })
